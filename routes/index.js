135f9d92adc2d592db840d5891f63aa90788e439
Oswaldo Bejarano <oabejaranoh@gmail.com>
2017-01-02 23:45:28 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+//var app = express();
+
+
+var execGenARIS = function() {
+    console.log('I come from the frontend ARIS Gen')
+};
+
+var execGenLeanIX= function() {
+    console.log('I come from the frontend LeanIX Gen')
+};
+
+var execLoadARIS = function() {
+    console.log('I come from the frontend ARIS Load')
+};
+
+var execLoadLeanIX = function() {
+    console.log('I come from the frontend LeanIX Load')
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ARIS-LeanIX MAP' });
+});
+
+
+router.post('/', function(req, res) {
+    var button1 = req.body.genARIScsv;
+    var button2 = req.body.genLeanIXcsv;
+    var button3 = req.body.loadARIScsv;
+    var button4 = req.body.loadLeanIXcsv;
+
+    console.log(req.body);
+    res.sendStatus(200);
+
+    if (button1 == "Generate"){
+        execGenARIS();
+    }
+    if (button2 == "Generate"){
+        execGenLeanIX();
+    }
+    if (button3 == "Load"){
+        execLoadARIS();
+    }
+    if (button4 == "Load"){
+        execLoadLeanIX();
+    }
+});
+
+module.exports = router;
